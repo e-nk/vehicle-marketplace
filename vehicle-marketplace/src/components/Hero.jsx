cbf0acced15a92d7e22465faedb15b508470d93d
e-nk <enocklagat747@gmail.com>
2024-12-06 01:32:02 +0300

    Data(car make & pricing), updated hero slide

--- vehicle-marketplace/src/components/Hero.jsx
+++ vehicle-marketplace/src/components/Hero.jsx
@@ -3,11 +3,12 @@ import Search from './Search'
 
 function Hero() {
 	return (
-		<div className='flex flex-col items-center p-10 py-20 gap-6 h-[600px] w-full bg-[#eef0fc]'>
+		<div className='flex flex-col items-center p-10 py-20 gap-6 h-[650px] w-full bg-[#eef0fc]'>
 			<h2 className='text-lg'>Find a car for sale or rent near you</h2>
 			<h2 className='text-[60px] font-bold'>Find Your Dream Car</h2>
-			
+
 			<Search/>
+			<img src='src/assets/tesla.png' className='mt-10'/>
 		</div>
 	)
 }
---
+++ vehicle-marketplace/src/components/Shared/Data.jsx
@@ -0,0 +1,130 @@
+const CarMakes=[
+	{
+		id:1,
+		name: 'Audi'
+	},
+	{
+		id:2,
+    name: 'BMW'
+	},
+	{
+		id:3,
+    name: 'Mercedes-Benz'
+	},
+	{
+		id:4,
+    name: 'Volkswagen'
+	},
+	{
+		id:5,
+    name: 'Toyota'
+	},
+	{
+		id:6,
+    name: 'Honda'
+	},
+	{
+		id:7,
+    name: 'Ford'
+	},
+	{
+		id:8,
+    name: 'Nissan'
+	},
+	{
+		id:9,
+    name: 'Hyundai'
+	},
+	{
+		id:10,
+    name: 'Kia'
+	},
+	{
+		id:11,
+    name: 'Chevrolet'
+	},
+	{
+		id:12,
+    name: 'Mazda'
+	},
+	{
+		id:13,
+    name: 'Volvo'
+	},
+	{
+		id:14,
+    name: 'Dodge'
+	},
+	{
+		id:15,
+    name: 'Jaguar'
+	}
+]
+
+
+const Pricing=[
+	{
+		id:1,
+		amount: '$2500'
+	},
+	{
+		id:2,
+    amount: '$3000'
+	},
+	{
+		id:3,
+    amount: '$3500'
+	},
+	{
+		id:4,
+    amount: '$4000'
+	},
+	{
+		id:5,
+    amount: '$4500'
+	},
+	{
+		id:6,
+    amount: '$5000'
+	},
+	{
+		id:7,
+    amount: '$5500'
+	},
+	{
+		id:8,
+    amount: '$6000'
+	},
+	{
+		id:9,
+    amount: '$6500'
+	},
+	{
+		id:10,
+    amount: '$7000'
+	},
+	{
+		id:11,
+    amount: '$7500'
+	},
+	{
+		id:12,
+    amount: '$8000'
+	},
+	{
+		id:13,
+    amount: '$8500'
+	},
+	{
+		id:14,
+    amount: '$9000'
+	},
+	{
+		id:15,
+    amount: '$9500'
+	}
+]
+
+export default{
+	CarMakes, Pricing
+}
