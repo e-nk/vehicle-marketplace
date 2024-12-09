cb94d77bb1d57738a4a4623d5db346f053c914ec
e-nk <enocklagat747@gmail.com>
2024-12-09 22:28:22 +0300

    car item link to page

--- vehicle-marketplace/src/listing-details/[id]/index.jsx
+++ vehicle-marketplace/src/listing-details/[id]/index.jsx
@@ -1,8 +1,19 @@
+import Header from '@/components/Header'
 import React from 'react'
+import DetailHeader from './components/DetailHeader'
 
 function ListingDetail() {
 	return (
-		<div>ListingDetail</div>
+		<div>
+			<Header/>
+
+			<div className='p-10 md:px-20'>
+				{/* <Header detail component*/}
+				<DetailHeader/>
+
+			</div>
+
+		</div>
 	)
 }
 
