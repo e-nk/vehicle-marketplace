5d6222c00919da2b0530694f0703e8a1870d93cf
e-nk <enocklagat747@gmail.com>
2025-01-06 15:48:42 +0300

    Logo Fix

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { UserButton,useUser } from '@clerk/clerk-react';
 import { Button } from './ui/button';
 import { Link } from 'react-router-dom';
+import Image from "next/image";
 
 
 
